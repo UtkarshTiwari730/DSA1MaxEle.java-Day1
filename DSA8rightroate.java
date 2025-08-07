23751fec16f14daab8fdc0bddf172c35f41517cc
utkarsh tiwari <utkarsht276@gmail.com>
2025-08-07 23:51:06 +0530

    Add files via upload
    same as left roate only change is to store arr.lemgth-1 in temp

---
+++ DSA8rightroate.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+
+public class DSA8rightroate {
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+
+        int temp=arr[arr.length-1];
+
+        for(int i=arr.length-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+System.out.println(Arrays.toString(arr));
+    }
+    
+}
