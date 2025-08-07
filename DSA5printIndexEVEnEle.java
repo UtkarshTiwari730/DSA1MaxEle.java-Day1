47a5433273403cc3cbdc6f3a831602e8ac859710
utkarsh tiwari <utkarsht276@gmail.com>
2025-08-07 11:27:15 +0530

    Add files via upload
    in this we have to print the even value of zero index based 
    ex 0,2,4,6....... by doing i+=2

---
+++ DSA5printIndexEVEnEle.java
@@ -0,0 +1,10 @@
+public class DSA5printIndexEVEnEle {
+    public static void main(String args[]){
+        int arr[]={1,3,4,6,8,4,5};
+
+        for(int i=0;i<arr.length;i+=2){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
