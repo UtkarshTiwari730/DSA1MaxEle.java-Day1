2c07db10eadc789959e041603dd079d84f071a22
utkarsh tiwari <utkarsht276@gmail.com>
2025-08-07 09:59:25 +0530

    Add files via upload
    this is a day one of series were i find a maximum element of an array..
    ex  int  arr[1,2,3,4,8,5,7]
    output=8;

---
+++ DSA1MaxEle.java
@@ -0,0 +1,14 @@
+public class DSA1MaxEle {
+    public static void main(String[]args){
+        int arr[]={-2,12,5,9,7,6};
+        int max=arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+    
+}
